2573b8118a4d5e747d458af0999108b7eef7a0c8
qbinh1423 <140331167+qbinh1423@users.noreply.github.com>
2024-09-12 23:03:29 +0700

    Standardize JSON responses

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+
+const JSend = require("./jsend");
 const contactsRouter = require("./routes/contacts.router");
 
 const app = express();
@@ -9,9 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  return res.json({
-    message: "ok",
-  });
+  return res.json(JSend.success());
 });
 
 contactsRouter.setup(app);
